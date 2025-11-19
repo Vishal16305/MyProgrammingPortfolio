61fd07d4e528d4869e1cfbca69bb510ddaf87fd0
Vishal16305 <vistiw2005@gmail.com>
2025-11-19 23:51:09 +0530

    Add TwoSum function in twosum.cpp
    Implement TwoSum function to find indices of two numbers that add up to target.

---
+++ twosum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+
+public:
+
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+
+        std::unordered_map<int, int> map;
+
+        for (int i = 0; i < nums.size(); ++i) {
+
+            int complement = target - nums[i];
+
+            if (map.count(complement)) {
+
+                return {map[complement], i};
+
+            }
+
+            map[nums[i]] = i;
+
+        }
+
+        return {}; // Should not reach here as per problem description
+
+    }
+
+};
+
